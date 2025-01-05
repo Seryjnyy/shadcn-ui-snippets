f91aa5cb4827cccd7ef30c958c0cc92d1ae5f8b5
Jakub Wojcik <jakubwojcik6a@gmail.com>
2025-01-05 23:39:31 +0000

    Add function to move docs from repo to project

--- scripts/python-test/fetch_shadcn_components.py
+++ scripts/python-test/fetch_shadcn_components.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import config
+import shutil
 
 # Directory for the templates repo
 REPO_DIR = "../shadcn-ui-repo"
@@ -43,4 +44,44 @@ def fetch_shadcn_components():
         print("Repository exists. Pulling latest changes...")
         run_command(["git", "-C", REPO_DIR, "pull"], cwd=REPO_DIR)
 
-print("Repository is up to date.")
+    print("Repository is up to date.")
+
+
+# duplicate code
+def copy_files(file_list, target_directory, overwrite=False):
+    """
+    Copies a list of files to a target directory.
+
+    :param file_list: List of file paths to copy.
+    :param target_directory: Directory where files will be copied.
+    """
+    # Ensure the target directory exists
+    os.makedirs(target_directory, exist_ok=True)
+
+    for file_path in file_list:
+
+        try:
+            # Copy each file to the target directory
+            if overwrite or not os.path.exists(
+                os.path.join(target_directory, os.path.basename(file_path))
+            ):
+                shutil.copy(file_path, target_directory)
+                print(f"Copied: {file_path} -> {target_directory}")
+            else:
+                print(f"File already exists: {file_path}")
+        except FileNotFoundError:
+            print(f"File not found: {file_path}")
+        except Exception as e:
+            print(f"Error copying {file_path}: {e}")
+
+
+def move_from_repo_to_project():
+    pathToDir = REPO_DIR + DIR_COMPONENTS_IN_REPO
+
+    files_and_dirs = os.listdir(pathToDir)
+
+    full_paths = [
+        os.path.join(pathToDir, file_or_dir) for file_or_dir in files_and_dirs
+    ]
+
+    copy_files(full_paths, config.DIR_DOCS_FROM_REPO)
