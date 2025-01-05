75df761452a11f91ab958fee4b364497765cf4e3
Jakub Wojcik <jakubwojcik6a@gmail.com>
2025-01-05 23:21:52 +0000

    Add config file
    - Added directories.

---
+++ scripts/python-test/config.py
@@ -0,0 +1,7 @@
+DIR_DOCS_FROM_REPO = "1_components-from-repo"
+DIR_AUTO_DOCS = "2_auto-docs"
+DIR_MANUAL_DOCS = "2_manual-docs"
+DIR_GENERATED_JSONS = "3_generated-jsons"
+DIR_GENERATED_LIVE_TEMPLATES = "4_generated-live-templates"
+
+DIR_PLUGIN_RESOURCE_FOLDER = "../../src/main/resources/liveTemplates"
